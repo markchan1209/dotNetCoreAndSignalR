9a62f6cbac6328479fa6eea698dd5d5dc05d7caf
markchan <markchan@xsg.com.tw>
2018-09-11 18:44:20 +0800

    聊天室

---
+++ dotNetCoreAndSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessageToGroup(string groupName, string message)
+        {
+            return Clients.Groups(groupName).SendAsync("Send", $"{Context.ConnectionId}:{message}");
+        }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Groups(groupName).SendAsync("Send", $"{Context.ConnectionId} 加入房間 {groupName}");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Groups(groupName).SendAsync("Send", $"{Context.ConnectionId} 離開房間 {groupName}");
+        }
+    }
+}
---
+++ dotNetCoreAndSignalR/wwwroot/js/chat.js
@@ -0,0 +1,49 @@
+﻿// 建立連線
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+// 發送訊息
+connection.on("Send",
+    function (message) {
+        var li = document.createElement("li");
+        li.textContent = message;
+        document.getElementById("messagesList").appendChild(li);
+    });
+
+document.getElementById("groupmsg").addEventListener("click", async (event) => {
+    var groupName = document.getElementById("group-name").value;
+    var groupMsg = document.getElementById("group-message-text").value;
+    try {
+        await connection.invoke("SendMessageToGroup", groupName, groupMsg);
+    } catch (e) {
+        console.error(e);
+    }
+    event.preventDefault();
+});
+document.getElementById("join-group").addEventListener("click", async (event) => {
+    var groupName = document.getElementById("group-name").value;
+    try {
+        await connection.invoke("AddToGroup", groupName);
+    } catch (e) {
+        console.error(e);
+    }
+    event.preventDefault();
+});
+
+document.getElementById("leave-group").addEventListener("click", async (event) => {
+    var groupName = document.getElementById("group-name").value;
+    try {
+        await connection.invoke("RemoveFromGroup", groupName);
+        var foo = document.getElementById('messagesList');
+        while (foo.firstChild) foo.removeChild(foo.firstChild);
+    } catch (e) {
+        console.error(e);
+    }
+    event.preventDefault();
+});
+(async () => {
+    try {
+        await connection.start();
+    } catch (e) {
+        console.error(e.toString());
+    }
+})();
